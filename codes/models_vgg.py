fa1026911d98945974c28bdc7230db18b9e0e41f
Junsuk Choe <skykite@yonsei.ac.kr>
2018-08-07 21:38:42 +0900

    fix code bug

--- codes/models_vgg.py
+++ codes/models_vgg.py
@@ -16,21 +16,21 @@ def vgg_gap(image, sn=True):
 
     l = tf.nn.relu(Spec_Conv2D('conv1_1', image, 64, sn=sn), name='conv1_1_relu')
     l = tf.nn.relu(Spec_Conv2D('conv1_2', l, 64, sn=sn), name='conv1_2_relu')
-    l = tf.nn.max_pool(l, ksize=[1,2,2,1], stride=[1,2,2,1], padding='SAME', name='pool1')
+    l = tf.nn.max_pool(l, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME', name='pool1')
 
     l = tf.nn.relu(Spec_Conv2D('conv2_1', l, 128, sn=sn), name='conv2_1_relu')
     l = tf.nn.relu(Spec_Conv2D('conv2_2', l, 128, sn=sn), name='conv2_2_relu')
-    l = tf.nn.max_pool(l, ksize=[1,2,2,1], stride=[1,2,2,1], padding='SAME', name='pool2')
+    l = tf.nn.max_pool(l, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME', name='pool2')
 
     l = tf.nn.relu(Spec_Conv2D('conv3_1', l, 256, sn=sn), name='conv3_1_relu')
     l = tf.nn.relu(Spec_Conv2D('conv3_2', l, 256, sn=sn), name='conv3_2_relu')
     l = tf.nn.relu(Spec_Conv2D('conv3_3', l, 256, sn=sn), name='conv3_3_relu')
-    l = tf.nn.max_pool(l, ksize=[1,2,2,1], stride=[1,2,2,1], padding='SAME', name='pool3')
+    l = tf.nn.max_pool(l, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME', name='pool3')
 
     l = tf.nn.relu(Spec_Conv2D('conv4_1', l, 512, sn=sn), name='conv4_1_relu')
     l = tf.nn.relu(Spec_Conv2D('conv4_2', l, 512, sn=sn), name='conv4_2_relu')
     l = tf.nn.relu(Spec_Conv2D('conv4_3', l, 512, sn=sn), name='conv4_3_relu')
-    l = tf.nn.max_pool(l, ksize=[1,2,2,1], stride=[1,2,2,1], padding='SAME', name='pool4')
+    l = tf.nn.max_pool(l, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME', name='pool4')
 
     l = tf.nn.relu(Spec_Conv2D('conv5_1', l, 512, sn=sn), name='conv5_1_relu')
     l = tf.nn.relu(Spec_Conv2D('conv5_2', l, 512, sn=sn), name='conv5_2_relu')
