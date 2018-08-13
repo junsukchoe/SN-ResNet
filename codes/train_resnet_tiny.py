8e5fb49dfcd4e7b44f49535abafe9aee62583cff
Junsuk Choe <skykite@yonsei.ac.kr>
2018-08-13 14:46:05 +0900

    fix code bug

--- codes/train_resnet_tiny.py
+++ codes/train_resnet_tiny.py
@@ -69,7 +69,7 @@ class Model(ModelDesc):
             convmaps_target = preresnet_group(
                     'group3new', convmaps, 512, defs[3], 1, isTrain, args.sn)
             convmaps_gap = tf.reduce_mean(convmaps_target, [1,2], name='gap')
-            logits, w = FullyConnected('linearnew', convmaps_gap, 200)
+            logits = FullyConnected('linearnew', convmaps_gap, 200)
 
         weights = tf.identity(w, name='linearweight')
         activation_map = tf.identity(convmaps_target, name='actmap')
